2eb2785964b8b91bdcc5f70c3b4220108d2cc44c
rijuvijayan <rijuvijayan@gmail.com>
2020-07-06 11:33:00 +1200

    gotten rid of unwanted element

--- app.js
+++ app.js
@@ -9,5 +9,4 @@ request({ url: url }, (error, response, body) => {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body);
-  cle;
 });

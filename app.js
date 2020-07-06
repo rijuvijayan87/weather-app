058d0fb27be389dbf5c2bc217da59cb9179ce8bd
rijuvijayan <rijuvijayan@gmail.com>
2020-07-06 15:47:04 +1200

    updated app.js with chaining and destructuring objects

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Wellington', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
+const address = process.argv[2];
+
+geocode(address, (error, { latitude, longitude, location }) => {
+  if (error) {
+    return console.log(error);
+  }
+  forecast(latitude, longitude, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Location :', location);
+    console.log('Data:', data);
+  });
 });
 
-forecast(-41.28889, 174.77722, (error, data) => {
-  console.log('Error:', error);
-  console.log('Data:', data);
-});
+// const product = {
+//   label: 'test label',
+//   price: 3,
+//   stock: 201,
+//   salePrice: 33,
+//   rating: 4.2,
+// };
+
+// const { label, price } = product;
+// console.log(label, price);
+
+// const transaction = (type, { label, price, stock }) => {
+//   console.log(type, label, price, stock);
+// };
+
+// transaction('order', product);
